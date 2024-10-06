abe8ed42c32ec8526ae6b2bab6e9cc416b49e0e4
Ricardo Contreras Santos <zS22024113@estudiantes.uv.mx>
2024-10-06 02:16:38 -0600

    Se hizo una modificación en el settings para poder leer el djangorestframework

--- settings.py
+++ settings.py
@@ -57,6 +57,15 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
